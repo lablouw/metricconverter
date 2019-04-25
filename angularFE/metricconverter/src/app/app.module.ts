b25f2ebb45c01519d6610055a13fcde26a9953de
lloydlouw <lablouw@gmail.com>
2019-04-25 02:19:54 +0200

    removed components

--- angularFE/metricconverter/src/app/app.module.ts
+++ angularFE/metricconverter/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
-import { TemperatureUnitComponent } from './temperature-unit/temperature-unit.component';
-import { MassUnitComponent } from './mass-unit/mass-unit.component';
-import { DistanceUnitComponent } from './distance-unit/distance-unit.component';
 
 const appRoutes: Routes = [
   {
