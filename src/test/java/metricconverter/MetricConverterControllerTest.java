bc6e3a6dd3af908db5e7c57ebf10bb850a8dda6e
lloydlouw <lablouw@gmail.com>
2019-04-26 08:39:32 +0200

    unit tests

--- src/test/java/metricconverter/MetricConverterControllerTest.java
+++ src/test/java/metricconverter/MetricConverterControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.StringUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -41,5 +42,50 @@ public class MetricConverterControllerTest {
         }
     }
 
-    //Todo: other tests
+    @Test
+    public void test_convertMass() throws Exception {
+        MassUnit base = MassUnit.KILOGRAM;
+        for (MassUnit unit : MassUnit.values()) {
+            String url = "/convertMass?mass=1&fromUnit="+unit+"&toUnit="+base;
+            System.out.println("Testing: "+url);
+            mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo(String.valueOf(Units.getMassUnits().get(unit)))));
+        }
+    }
+
+    @Test
+    public void test_convertTemperature() throws Exception {
+        double c = 0;
+        double k = 273.15;
+        double f = 32;
+
+        String baseUrl = "/convertTemperature?temperature={temp}&fromUnit={from}&toUnit={to}";
+
+        String url = baseUrl.replace("{temp}", String.valueOf(c))
+                .replace("{from}", TemperatureUnit.CELSIUS.name())
+                .replace("{to}", TemperatureUnit.FAHRENHEIT.name());
+        mvc.perform(MockMvcRequestBuilders.get(url)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(String.valueOf(f))));
+
+        url = baseUrl.replace("{temp}", String.valueOf(c))
+                .replace("{from}", TemperatureUnit.CELSIUS.name())
+                .replace("{to}", TemperatureUnit.KELVIN.name());
+        mvc.perform(MockMvcRequestBuilders.get(url)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(String.valueOf(k))));
+
+        url = baseUrl.replace("{temp}", String.valueOf(k))
+                .replace("{from}", TemperatureUnit.KELVIN.name())
+                .replace("{to}", TemperatureUnit.FAHRENHEIT.name());
+        mvc.perform(MockMvcRequestBuilders.get(String.format(url, k, TemperatureUnit.KELVIN, TemperatureUnit.FAHRENHEIT))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(String.valueOf(f))));
+
+    }
+
 }
