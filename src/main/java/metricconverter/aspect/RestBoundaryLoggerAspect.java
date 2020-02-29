7e242d4117f442c2761e32163ebf776ada012f77
lloydlouw <lablouw@gmail.com>
2020-02-29 09:55:50 +0200

    Better logging

--- src/main/java/metricconverter/aspect/RestBoundaryLoggerAspect.java
+++ src/main/java/metricconverter/aspect/RestBoundaryLoggerAspect.java
@@ -15,14 +15,15 @@ public class RestBoundaryLoggerAspect {
         for (int i = 0; i < pjp.getArgs().length; i++) {
             argsPattern += "{},";
         }
-
         argsPattern = StringUtils.isEmpty(argsPattern) ? argsPattern : argsPattern.substring(0, argsPattern.length() - 1);
 
-        log.info("Inbound " + pjp.getSignature().getName() + (StringUtils.isEmpty(argsPattern) ? "" : " [args=" + argsPattern + "]"), pjp.getArgs());
+        String method = pjp.getSignature().getName();
+
+        log.info("Inbound: [method=" + method + (StringUtils.isEmpty(argsPattern) ? "]" : ", args=" + argsPattern + "]"), pjp.getArgs());
 
         try {
             Object ret = pjp.proceed();
-            log.info("Outbound " + pjp.getSignature().getName() + (ret == null ? "" : " [response={}]"), ret);
+            log.info("Outbound: [method=" + method + (ret == null ? "]" : ", response={}]"), ret);
             return ret;
         } catch (Throwable throwable) {
             log.error("Error in method " + pjp.getSignature().getName(), throwable);
